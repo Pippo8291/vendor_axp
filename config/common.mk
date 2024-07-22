f7d2269da1af8820451ab59cd78f0c0ce692ca29
steadfasterX <steadfasterX@gmail.com>
2024-07-22 07:34:39 +0000

    overlay: apply magisk hint on A10 and lower
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -3,13 +3,17 @@
 # This file is part of AXP.OS (https://axp.binbash.rocks)
 # LICENSE: GPLv3  (https://www.gnu.org/licenses/gpl-3.0.txt)
 #
-# Copyright (C) 2023 steadfasterX <steadfasterX -AT- gmail #DOT# com>
-# Copyright (C) 2024 steadfasterX <steadfasterX -AT- gmail #DOT# com>
+# Copyright (C) 2023-2024 steadfasterX <steadfasterX -AT- gmail #DOT# com>
 #
 ##############################################################################
 
+ifeq ($(call math_gt_or_eq,$(PLATFORM_SDK_VERSION),30), true)
 DEVICE_PACKAGE_OVERLAYS += \
     vendor/axp/overlays
+else
+DEVICE_PACKAGE_OVERLAYS += \
+    vendor/axp/overlays-10
+endif
 
 $(call inherit-product, vendor/axp/axp-vendor.mk)
 $(call inherit-product, vendor/extendrom/config/common.mk)
