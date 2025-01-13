aca899b0cc20f65861fa93585e14bb82b14a3867
steadfasterX <steadfasterX@users.noreply.github.com>
2025-01-13 09:43:52 +0100

    scripts: support arch(like) in semaphore updater

--- scripts/update_semaphore.sh
+++ scripts/update_semaphore.sh
@@ -4,15 +4,18 @@
 # License: GPLv2
 #############################################################################
 #
-# script version 2.0.2
+# script version 2.0.3
 
 VER=$1
 
 WHATOS=$(source /etc/os-release && echo $ID)
 
+[ $(id -u) != 0 ] && echo must be run with sudo && exit 4
+
 case $WHATOS in
     *buntu|[Dd]ebian) OSPKG=deb PKGINSTCMD="dpkg -i" ;;
     [Rr]ed[Hh]at|[Aa]lma*) OSPKG=rpm PKGINSTCMD="rpm -U";;
+    [Mm]anjaro|[Aa]rch) OSPKG=tar.gz PKGINSTCMD="" ;;
     *)echo "unknown OS ($WHATOS)" && exit 4;;
 esac
 
@@ -21,6 +24,7 @@ echo "supported OS detected: $WHATOS"
 
 BURL="https://github.com/ansible-semaphore/semaphore/releases/download/v${VER}/semaphore_${VER}_linux_amd64.${OSPKG}"
 XBIN=semaphore
+SVCUSER=semaphore
 
 [ -z "$VER" ] && echo "version missing (X.X.X) - no 'v' prefix!" && exit 4
 
@@ -31,15 +35,20 @@ if [ "$2" == "force" ];then
     esac
 fi
 
+if [ "$OSPKG" == "tar.gz" ];then
+    cd /tmp
+    tar xzf ${XBIN}.${OSPKG} semaphore
+fi  
+
 echo "starting SQL backup"
-/etc/cron.daily/semaphore-backup || exit $?
+#/etc/cron.daily/semaphore-backup || exit $?
 echo "finished SQL backup"
 
 wget -qO /tmp/${XBIN}.${OSPKG} "$BURL" \
     && echo "downloaded ${XBIN} - version: $VER" \
     && echo "stopping ${XBIN} ........." \
     && systemctl stop ${XBIN}.service \
-    && $PKGINSTCMD /tmp/${XBIN}.${OSPKG} \
+    && if [ "$OSPKG" == "tar.gz" ];then mv /tmp/$XBIN /opt/semaphore/ && chmod 750 /opt/semaphore/$XBIN && chown $SVCUSER /opt/semaphore/$XBIN; else $PKGINSTCMD /tmp/${XBIN}.${OSPKG};fi \
     && rm /tmp/${XBIN}.${OSPKG} \
     && echo "starting ${XBIN} - $VER" \
     && systemctl start ${XBIN}.service \
