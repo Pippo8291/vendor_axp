f2346d2725ca763d0128ddbc55892cb793ba81ae
steadfasterX <steadfasterX@gmail.com>
2025-04-16 10:38:10 +0200

    common: update comments
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -3,9 +3,11 @@
 # This file is part of AXP.OS (https://axp.binbash.rocks)
 # LICENSE: GPLv3  (https://www.gnu.org/licenses/gpl-3.0.txt)
 #
-# Copyright (C) 2023-2024 steadfasterX <steadfasterX -AT- gmail #DOT# com>
+# Copyright (C) 2023-2025 steadfasterX <steadfasterX -AT- gmail #DOT# com>
 #
 ##############################################################################
+
+$(call inherit-product, vendor/axp/axp-vendor.mk)
 
 # use correct overlay based on android SDK
 ifeq ($(call math_gt_or_eq,$(PLATFORM_SDK_VERSION),31), true)
@@ -19,7 +21,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     vendor/axp/overlays-10
 endif
 
-$(call inherit-product, vendor/axp/axp-vendor.mk)
+# include https://github.com/sfX-android/android_vendor_extendrom
 $(call inherit-product, vendor/extendrom/config/common.mk)
 
 # load the AXP.OS advanced AVB handling
