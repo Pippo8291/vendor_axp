882c7081a773d0da45b8173402a5ec2e5661621a
steadfasterX <steadfasterX@gmail.com>
2025-04-16 10:44:04 +0200

    board: heavy re-work:
    - override BOARD_AVB_SYSTEM_KEY_PATH - if previously defined (e.g. FP3)
    - added hash algo override for: vendor_boot, init_boot, vendor_kernel, pvmfw - if defined (e.g. komodo)
    - introducing: AXP_LOWEND_DEVICE - if defined: using sha256 (instead of sha512) for the hashtree algorithm of all partitions
    
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,17 +1,29 @@
-#####################################################################################
+#########################################################################################################
 #
 # This file is part of AXP.OS (https://axpos.org)
 # LICENSE: GPLv3 (https://www.gnu.org/licenses/gpl-3.0.txt)
 #
 # Copyright (C) 2023-2025 steadfasterX <steadfasterX -AT- gmail #DOT# com>
 #
-#####################################################################################
+#########################################################################################################
 # AXP.OS advanced AVB handling
 #
 # verification:
 # $> external/avb/avbtool info_image --image vbmeta.img
 # $> external/avb/avbtool verify_image --follow_chain_partitions --image vbmeta.img
-#####################################################################################
+#########################################################################################################
+#
+# !!!!!!!! IMPORTANT !!!!!!!!!!
+#
+# in order to make use of the required conditions this BoardConfig must be explicitly
+# included in the target's own BoardConfig.mk - at the bottom (must be the last line)!
+# Here a copy template for the device tree:
+#
+# # even though we include vendor/axp/config/common.mk we need to include BoardConfig (after the above
+# # definitions & includes), too so we we can make use of the conditions within
+# include vendor/axp/BoardConfigVendor.mk
+#
+#########################################################################################################
 
 # Enable android verified boot
 BOARD_AVB_ENABLE := true
@@ -23,7 +35,7 @@ BOARD_AVB_ALGORITHM := SHA512_RSA4096
 # see https://axpos.org/Bootloader-Lock
 BOARD_AVB_KEY_PATH := user-keys/avb.pem
 
-# BOARD_AVB_RECOVERY_KEY_PATH must be defined for if non-A/B is supported.
+# BOARD_AVB_RECOVERY_KEY_PATH must be defined for if non-A/B is supported. e.g. klte
 # See https://android.googlesource.com/platform/external/avb/+/master/README.md#booting-into-recovery
 ifeq ($(TARGET_OTA_ALLOW_NON_AB),true)
 ifneq ($(INSTALLED_RECOVERYIMAGE_TARGET),)
@@ -33,6 +45,12 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 endif
 endif
 
+# overwrite testkeys on system partition if defined (e.g. FP3)
+ifdef BOARD_AVB_SYSTEM_KEY_PATH
+BOARD_AVB_SYSTEM_KEY_PATH := $(BOARD_AVB_KEY_PATH)
+endif
+
+# board algorithms
 BOARD_AVB_BOOT_ALGORITHM := $(BOARD_AVB_ALGORITHM)
 BOARD_AVB_RECOVERY_ALGORITHM := $(BOARD_AVB_ALGORITHM)
 BOARD_AVB_SYSTEM_ALGORITHM := $(BOARD_AVB_ALGORITHM)
@@ -58,8 +76,12 @@ CUSTOM_IMAGE_AVB_ALGORITHM := $(BOARD_AVB_ALGORITHM)
 #BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 #BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
 
-# Using sha512 for the hashtree of all partitions
+# Using either sha256 or sha512 for the hashtree of all partitions depending on the device performance
+ifdef AXP_LOWEND_DEVICE
+TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM := sha256
+else
 TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM := sha512
+endif
 
 # overwrite general hashtree algorithms
 TARGET_AVB_SYSTEM_HASHTREE_ALGORITHM := $(TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM)
@@ -70,15 +92,35 @@ TARGET_AVB_SYSTEM_DLKM_HASHTREE_ALGORITHM := $(TARGET_AVB_GLOBAL_HASHTREE_ALGORI
 # enforce global hashtree footer algorithm for system
 BOARD_AVB_SYSTEM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm $(TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM)
 
+# if required, enforce global hash footer algorithm for vendor_boot
+ifdef BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE
+BOARD_AVB_VENDOR_BOOT_ADD_HASH_FOOTER_ARGS += --hash_algorithm $(TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM)
+endif
+
+# if required, enforce global hash footer algorithm for init_boot
+ifdef BOARD_INIT_BOOT_IMAGE_PARTITION_SIZE
+BOARD_AVB_INIT_BOOT_ADD_HASH_FOOTER_ARGS += --hash_algorithm $(TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM)
+endif
+
+# if required, enforce global hash footer algorithm for vendor_kernel
+ifdef BOARD_VENDOR_KERNEL_BOOTIMAGE_PARTITION_SIZE
+BOARD_AVB_VENDOR_KERNEL_BOOT_ADD_HASH_FOOTER_ARGS += --hash_algorithm $(TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM)
+endif
+
+# if required, enforce global hash footer algorithm for pvmfw
+ifdef BOARD_PVMFWIMAGE_PARTITION_SIZE
+BOARD_AVB_PVMFW_ADD_HASH_FOOTER_ARGS += --hash_algorithm $(TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM)
+endif
+
 # FP3 breaks when adding hashtree footers (at least on boot + dtbo) so filter it out when detected
-# likely it could be enabled on all other partitions but this wasn't tested
-#ifeq ($(filter FP3,$(BDEVICE)),) # <-- cant get this working
-ifneq ($(BDEVICE),FP3)   # <-- likely we need to identify the root cause for this, i.e. e.g. "if chaining"?
+# likely it could be enabled on the other partitions but this wasn't tested
+ifeq ($(filter FP3,$(TARGET_DEVICE)),) # <-- likely we need to identify the root cause for this, i.e. e.g. "if chaining"?
 
 # enforce global hashtree algorithm for boot, dtbo, recovery, system, system_other|ext|dlkm, product
 BOARD_AVB_BOOT_ADD_HASH_FOOTER_ARGS += --hash_algorithm $(TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM)
 BOARD_AVB_DTBO_ADD_HASH_FOOTER_ARGS += --hash_algorithm $(TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM)
 
+# enforce global hashtree algorithm for recovery but only when there is a dedicated recovery
 ifneq ($(TARGET_NO_RECOVERY),true)
 BOARD_AVB_RECOVERY_ADD_HASH_FOOTER_ARGS += --hash_algorithm $(TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM)
 endif
@@ -96,9 +138,4 @@ BOARD_AVB_VENDOR_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm $(TARGET_AVB_GLOBA
 BOARD_AVB_ODM_DLKM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm $(TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM)
 BOARD_AVB_VENDOR_DLKM_ADD_HASHTREE_FOOTER_ARGS += --hash_algorithm $(TARGET_AVB_GLOBAL_HASHTREE_ALGORITHM)
 
-else
-
-# overwrite testkeys on system partition if defined (e.g. FP3)
-BOARD_AVB_SYSTEM_KEY_PATH := $(BOARD_AVB_KEY_PATH)
-
-endif # ifeq FP3
+endif # ifeq filter FP3
